772357137f76f719166e5c8d4f37666469c1add0
John Aiken <johnm.aiken@gmail.com>
2015-08-19 17:38:34 +0900

    Create SchoolColors.py

---
+++ plotting/SchoolColors.py
@@ -0,0 +1,14 @@
+class SchoolColors:
+    def __init__(self):
+        self.CU_Boulder()
+    
+    def CU_Boulder(self):
+        self.CU_Gold = '#CFB87C'
+        self.CU_Black = '#000000'
+        self.CU_DarkGray = '#565A5C'
+        self.CU_LightGray = '#A2A4A3'
+        
+    def colorWheel(self):
+        """
+        Nothing here yet
+        """
